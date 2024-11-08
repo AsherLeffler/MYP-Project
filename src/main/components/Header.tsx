cf87413110860f9775166b2c2237444d10c2506e
Asher Leffler <asher.leffler@gmail.com>
2024-11-08 10:24:42 -0500

    Enhance header component with mobile navigation and dropdown menu; update styles for improved responsiveness

--- src/main/components/Header.tsx
+++ src/main/components/Header.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, Menu } from "lucide-react";
 import selfieImg from "/selfie.webp";
 import { useEffect } from "react";
 
@@ -27,54 +27,105 @@ const Header: React.FC<HomeProps> = ({ headerStates }) => {
         default:
           return "Home";
       }
-    };  
+    };
 
     const currentLocation = window.location.pathname;
     setActivePage(findPage(currentLocation));
   }, [setActivePage]);
 
+  const openMenu = () => {
+    const dropDown = document.getElementById("drop-down") as HTMLElement;
+    dropDown.classList.toggle("open");
+    const menuIcon = document.getElementById("menuIcon") as HTMLElement;
+    menuIcon.classList.toggle("open");
+  };
+
   return (
-    <header>
-      <Link
-        to={"/"}
-        onClick={() => {
-          window.location.reload();
-          setActivePage("Home");
-        }}
-      >
-        <div className="logo-wrapper">
-          <img src={selfieImg} alt="Picture of me" id="imgOfMe" />
-          <div id="expandBtn">
-            <p>Asher Leffler</p> <ArrowLeft size={28} color="white" />
-          </div>
-        </div>
-      </Link>
-      <nav>
+    <>
+      <header>
         <Link
           to={"/"}
-          className={activePage === "Home" ? "active" : ""}
-          onClick={() => setActivePage("Home")}
+          onClick={() => {
+            setActivePage("Home");
+            window.location.reload();
+          }}
         >
-          Home
+          <div className="logo-wrapper">
+            <img src={selfieImg} alt="Picture of me" id="imgOfMe" />
+            <div id="expandBtn">
+              <p>Asher Leffler</p> <ArrowLeft size={28} color="white" />
+            </div>
+          </div>
         </Link>
-        <div className="divider-circle"></div>
-        <Link
-          to={"/myJourney"}
-          className={activePage === "My Journey" ? "active" : ""}
-          onClick={() => setActivePage("My Journey")}
-        >
-          My Journey
-        </Link>
-        <div className="divider-circle"></div>
-        <Link
-          to={"/myFuture"}
-          className={activePage === "My Future" ? "active" : ""}
-          onClick={() => setActivePage("My Future")}
-        >
-          My Future
-        </Link>
-      </nav>
-    </header>
+        <nav className="desktop-nav">
+          <Link
+            to={"/"}
+            className={activePage === "Home" ? "active" : ""}
+            onClick={() => setActivePage("Home")}
+          >
+            Home
+          </Link>
+          <div className="divider-circle"></div>
+          <Link
+            to={"/myJourney"}
+            className={activePage === "My Journey" ? "active" : ""}
+            onClick={() => setActivePage("My Journey")}
+          >
+            My Journey
+          </Link>
+          <div className="divider-circle"></div>
+          <Link
+            to={"/myFuture"}
+            className={activePage === "My Future" ? "active" : ""}
+            onClick={() => setActivePage("My Future")}
+          >
+            My Future
+          </Link>
+        </nav>
+        <nav className="mobile-nav">
+          <button className="mobile-nav-button">
+            <Menu
+              onClick={openMenu}
+              id="menuIcon"
+              size={32}
+              color="white"
+              strokeWidth={1.5}
+            />
+          </button>
+        </nav>
+      </header>
+      <div id="drop-down">
+      <nav className="dropdown-nav">
+          <Link
+            to={"/"}
+            className={activePage === "Home" ? "active" : ""}
+            onClick={() => {
+              setActivePage("Home");
+            }}
+          >
+            Home
+          </Link>
+          <Link
+            to={"/myJourney"}
+            className={activePage === "My Journey" ? "active" : ""}
+            onClick={() => {
+              setActivePage("My Journey");
+            }}
+          >
+            My Journey
+          </Link>
+          <Link
+            to={"/myFuture"}
+            className={activePage === "My Future" ? "active" : ""}
+            onClick={() => {
+              setActivePage("My Future");
+            }}
+          >
+            My Future
+          </Link>
+        </nav>
+      </div>
+    </>
   );
 };
 
