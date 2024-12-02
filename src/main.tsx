3630f63742742fd11f2a90e0a793521354e79740
Asher Leffler <asher.leffler@gmail.com>
2024-12-02 13:58:53 -0500

    remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./main/App.tsx";
 import { BrowserRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </StrictMode>
+  <Router>
+    <App />
+  </Router>
 );
